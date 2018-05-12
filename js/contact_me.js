55412bbb188953b40ab5dfd857740833e9f5f632
Al <al@Als-Macbook.local>
2018-05-12 21:09:33 +1000

    contactform

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("#sendMessageButton");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "https://formspree.io/dangerall11@yahoo.co.uk",
+        url: "https://formspree.io/contactForm",
         type: "POST",
         data: $(this).serialize(),
         dataType: "json",
